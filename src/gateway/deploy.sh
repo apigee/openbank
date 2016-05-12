3ae5214298bd6b09b39a81d52749df0df578b130
Prashanth Subrahmanyam <psubrahmanyam@apigee.com>
2016-05-12 09:47:57 +0200

    parent pom maven dependency

--- src/gateway/deploy.sh
+++ src/gateway/deploy.sh
@@ -1,3 +1,3 @@
 cd $1
-mvn -e clean apigee-enterprise:deploy -P $2 -Dusername=psubrahmanyam@apigee.com -Dpassword=$3
+mvn -e clean apigee-enterprise:deploy -P $2 -Dusername=psubrahmanyam+apisbank@apigee.com -Dpassword=$3
 cd ..
