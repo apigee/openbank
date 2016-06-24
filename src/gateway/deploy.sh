f21d039b041c57d517543e96b4963e1a18e990ff
Prashanth Subrahmanyam <ksprashanth82@gmail.com>
2016-06-24 15:01:48 +0530

    Delete deploy.sh

--- src/gateway/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-cd $1
-cp ./config.orig ./config.json
-sed -i "" "s/__APPORG__/psubrahmanyam/g" ./config.json
-sed -i "" "s/__APPAPP__/apisbank/g" ./config.json
-mvn clean install -Dusername=$2 -Dpassword=$3 -Dorg=apis-bank -P$4
-cd ..
