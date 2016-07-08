dc431b7a1f7af9950cfe1a251012ef09691de9ab
Prashanth K S <prashanth@retina-koo-2.local>
2016-07-08 12:15:00 +0530

    updated

--- src/gateway/deploy.sh
+++ src/gateway/deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 cd $1
-cp ./config.orig ./config.json
+#cp ./config.orig ./config.json
 sed -i "" "s/__APPORG__/psubrahmanyam/g" ./config.json
 sed -i "" "s/__APPAPP__/apisbank/g" ./config.json
 mvn clean install -Dusername=$2 -Dpassword=$3 -Dorg=apis-bank -P$4
