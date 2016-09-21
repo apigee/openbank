8d5d60292f3a7dc5ac42ef73fd835e34225406e9
Prashanth K S <prashanth@retina-koo-3.local>
2016-09-21 11:06:49 +0530

    handle missing context variables in builduserinfo

--- src/gateway/userinfo/apiproxy/resources/jsc/BuildUserInfoResponse.js
+++ src/gateway/userinfo/apiproxy/resources/jsc/BuildUserInfoResponse.js
@@ -1,47 +1,56 @@
-var scope = context.getVariable("accesstoken.scope");
+var scope = getContext("accesstoken.scope");
+var grantType = getContext("grant_type");
+
+if (grantType === 'client_credentials')
+    scope = "profile email phone address";
+
+if (scope.indexOf("accounts") !== -1)
+    scope = "profile email phone address";
+
 var scopes;
 var userInfo = {};
 var address = {};
 
-var sub = context.getVariable("accesstoken.sub");
-var customer_id = context.getVariable("customer_id");
+var sub = getContext("accesstoken.sub");
+var customer_id = getContext("customer_id");
+if (!sub) sub = customer_id;
 
-var full_name = context.getVariable("full_name");
-var given_name = context.getVariable("given_name");
-var family_name = context.getVariable("family_name");
-var middle_name = context.getVariable("middle_name");
-var nickname = context.getVariable("nickname");
-var preferred_username = context.getVariable("preferred_username");
+var full_name = getContext("full_name");
+var given_name = getContext("given_name");
+var family_name = getContext("family_name");
+var middle_name = getContext("middle_name");
+var nickname = getContext("nickname");
+var preferred_username = getContext("preferred_username");
 
-var picture = context.getVariable("picture");
-var profile = context.getVariable("profile");
-var website = context.getVariable("website");
+var picture = getContext("picture");
+var profile = getContext("profile");
+var website = getContext("website");
 
-var gender = context.getVariable("gender");
-var birthdate = context.getVariable("birthdate");
-var zoneinfo = context.getVariable("zoneinfo");
-var locale = context.getVariable("locale");
+var gender = getContext("gender");
+var birthdate = getContext("birthdate");
+var zoneinfo = getContext("zoneinfo");
+var locale = getContext("locale");
 
-var updated_at = context.getVariable("updated_at");
+var updated_at = getContext("updated_at");
 
-var email = context.getVariable("email");
-var email_verified = context.getVariable("email_verified");
+var email = getContext("email");
+var email_verified = getContext("email_verified") || false;
 
-var phone_number = context.getVariable("phone_number");
-var phone_number_verified = context.getVariable("phone_number_verified");
+var phone_number = getContext("phone_number");
+var phone_number_verified = getContext("phone_number_verified") || false;
 
-var formatted = context.getVariable("formatted") || "";
-var house_name = context.getVariable("house_name") || "";
-var street = context.getVariable("street") || "";
-var locality = context.getVariable("locality") || "";
-var region = context.getVariable("region") || "";
-var postal_code = context.getVariable("postal_code") || "";
-var country = context.getVariable("country") || "";
+var formatted = getContext("formatted");
+var house_name = getContext("house_name");
+var street = getContext("street");
+var locality = getContext("locality");
+var region = getContext("region");
+var postal_code = getContext("postal_code");
+var country = getContext("country");
 
 if (scope != null) {
     scopes = scope.split(" ");
 
-    if (scopes.indexOf("profile") !== -1 || scopes.indexOf("accounts") !== -1) {
+    if (scopes.indexOf("profile") !== -1) {
         buildUserInfoObject("sub", sub);
         buildUserInfoObject("customer_id", sub);
         buildUserInfoObject("name", full_name);
@@ -59,22 +68,22 @@ if (scope != null) {
         buildUserInfoObject("birthdate", birthdate);
         buildUserInfoObject("zoneinfo", zoneinfo);
         buildUserInfoObject("locale", locale);
-
         buildUserInfoObject("updated_at", updated_at);
     }
 
-    if (scopes.indexOf("email") !== -1 || scopes.indexOf("accounts") !== -1) {
+    if (scopes.indexOf("email") !== -1) {
         buildUserInfoObject("email", email);
         buildUserInfoObject("email_verified", email_verified);
     }
 
-    if (scopes.indexOf("phone") !== -1 || scopes.indexOf("accounts") !== -1) {
+    if (scopes.indexOf("phone") !== -1) {
         buildUserInfoObject("phone_number", phone_number);
         buildUserInfoObject("phone_number_verified", phone_number_verified);
     }
 
-    if (scopes.indexOf("address") !== -1 || scopes.indexOf("accounts") !== -1) {
+    if (scopes.indexOf("address") !== -1) {
         userInfo.address = {};
+
         buildAddressObject("formatted", formatted);
         buildAddressObject("house_name", house_name);
         buildAddressObject("street", street);
@@ -85,17 +94,46 @@ if (scope != null) {
     }
 }
 
+// context.setVariable("jsonObject", userInfo);
+print(JSON.stringify(userInfo));
 context.setVariable("userInfoResponse", JSON.stringify(userInfo));
 
 function buildUserInfoObject(attribute, claim) {
-    // if (claim != null && claim != "") {
-        userInfo[attribute] = claim;
-    // }
+    try {
+        print(attribute + ' = ' + JSON.stringify(claim) + ' : ' + typeof claim);
+
+        if (typeof claim === 'object')
+            claim = "";
+
+        if (claim !== null && claim !== "" && claim !== 0) {
+            userInfo[attribute] = claim;
+        }
+    } catch (e) {
+        print('error = ' + e);
+    }
 }
 
 function buildAddressObject(attribute, claim) {
-    // if (claim != null && claim != "") {
-        userInfo.address[attribute] = claim;
-        print(userInfo.address[attribute]);
-    // }
-} 
+    try {
+        print(attribute + ' = ' + JSON.stringify(claim) + ' : ' + typeof claim);
+
+        if (typeof claim === 'object')
+            claim = "";
+
+        if (claim !== null && claim !== "" && claim !== 0) {
+            userInfo.address[attribute] = claim;
+        }
+    } catch (e) {
+        print('error = ' + e);
+    }
+}
+
+function getContext(name) {
+    var value = context.getVariable(name);
+    try {
+        if (value === null || typeof value === 'undefined')
+            value = null;
+    } catch (e) {
+        value = null;
+    }
+}
