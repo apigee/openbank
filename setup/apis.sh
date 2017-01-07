da296e9a1c556f20ea7908c0924467a2d252af0c
Prashanth Subrahmanyam <ksprashanth@google.com>
2017-01-08 01:56:25 +0800

    fixed setup script/

--- setup/apis.sh
+++ setup/apis.sh
@@ -3,40 +3,40 @@
 host=$ORG-$ENV.apigee.net
 
 #accounts-connector
-cp ../accounts-connector/config.orig ../accounts-connector/config.json
-sed -i "" "s/__APPORG__/$UGORG/g" ../accounts-connector/config.json
-sed -i "" "s/__APPAPP__/$UGAPP/g" ../accounts-connector/config.json
+cp src/gateway/accounts-connector/config.orig src/gateway/accounts-connector/config.json
+sed -i "s/__APPORG__/$UGORG/g" src/gateway/accounts-connector/config.json
+sed -i "s/__APPAPP__/$UGAPP/g" src/gateway/accounts-connector/config.json
 
 #authentication-connector
-cp ../authentication-connector/config.orig ../authentication-connector/config.json
-sed -i "" "s/__APPORG__/$UGORG/g" ../authentication-connector/config.json
-sed -i "" "s/__APPAPP__/$UGAPP/g" ../authentication-connector/config.json
+cp src/gateway/authentication-connector/config.orig src/gateway/authentication-connector/config.json
+sed -i "s/__APPORG__/$UGORG/g" src/gateway/authentication-connector/config.json
+sed -i "s/__APPAPP__/$UGAPP/g" src/gateway/authentication-connector/config.json
 
 #userinfo
-cp ../userinfo/config.orig ../userinfo/config.json
-sed -i "" "s/__APPORG__/$UGORG/g" ../userinfo/config.json
-sed -i "" "s/__APPAPP__/$UGAPP/g" ../userinfo/config.json
+cp src/gateway/userinfo/config.orig src/gateway/userinfo/config.json
+sed -i "s/__APPORG__/$UGORG/g" src/gateway/userinfo/config.json
+sed -i "s/__APPAPP__/$UGAPP/g" src/gateway/userinfo/config.json
 
 #locations-connector
-cp ../locations-connector/config.orig ../locations-connector/config.json
-sed -i "" "s/__APPORG__/$UGORG/g" ../locations-connector/config.json
-sed -i "" "s/__APPAPP__/$UGAPP/g" ../locations-connector/config.json
+cp src/gateway/locations-connector/config.orig src/gateway/locations-connector/config.json
+sed -i "s/__APPORG__/$UGORG/g" src/gateway/locations-connector/config.json
+sed -i "s/__APPAPP__/$UGAPP/g" src/gateway/locations-connector/config.json
 
 #transfers-connector
-cp ../transfers-connector/config.orig ../transfers-connector/config.json
-sed -i "" "s/__APPORG__/$UGORG/g" ../transfers-connector/config.json
-sed -i "" "s/__APPAPP__/$UGAPP/g" ../transfers-connector/config.json
+cp src/gateway/transfers-connector/config.orig src/gateway/transfers-connector/config.json
+sed -i "s/__APPORG__/$UGORG/g" src/gateway/transfers-connector/config.json
+sed -i "s/__APPAPP__/$UGAPP/g" src/gateway/transfers-connector/config.json
 
 #oauth
-cp ../oauth/config.orig ../oauth/config.json
-sed -i "" "s/__HOST__/$host/g" ../oauth/config.json
+cp src/gateway/oauth/config.orig src/gateway/oauth/config.json
+sed -i "s/__HOST__/$host/g" src/gateway/oauth/config.json
 
 #transfers
-cp ../transfers/config.orig ../transfers/config.json
-sed -i "" "s/__HOST__/$host/g" ../transfers/config.json
+cp src/gateway/transfers/config.orig src/gateway/transfers/config.json
+sed -i "s/__HOST__/$host/g" src/gateway/transfers/config.json
 # ticket: APIS-126 
-sed -i "" "s/__APPORG__/$UGORG/g" ../transfers-connector/apiproxy/policies/Callout.AddTransactionToAccount.xml
-sed -i "" "s/__APPAPP__/$UGAPP/g" ../transfers-connector/apiproxy/policies/Callout.AddTransactionToAccount.xml
+sed -i "s/__APPORG__/$UGORG/g" src/gateway/transfers-connector/apiproxy/policies/Callout.AddTransactionToAccount.xml
+sed -i "s/__APPAPP__/$UGAPP/g" src/gateway/transfers-connector/apiproxy/policies/Callout.AddTransactionToAccount.xml
 
 ### delete default oauth API
 echo `date`": Deleting oauth API !!"
@@ -45,6 +45,6 @@ echo "${SETUP_RESULT}"
 echo ""
 
 # deploy the APIs
-cd ../parent-pom/
+cd src/gateway/parent-pom/
 mvn clean install -Dusername=${ADMIN_EMAIL} -Dpassword=${APW} -Dorg=${ORG} -P${ENV}
-cd ../setup
+cd ../../..
--- setup/apps.sh
+++ setup/apps.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
 #consent-app
-cp ../consent-app/apiproxy/resources/node/config.orig ../consent-app/apiproxy/resources/node/config.json
-sed -i "" "s/__HOST__/$host/g" ../consent-app/apiproxy/resources/node/config.json
-sed -i "" "s/__APIINTKEY__/$INTKEY/g" ../consent-app/apiproxy/resources/node/config.json
+cp src/gateway/consent-app/apiproxy/resources/node/config.orig src/gateway/consent-app/apiproxy/resources/node/config.json
+sed -i "s/__HOST__/$host/g" src/gateway/consent-app/apiproxy/resources/node/config.json
+sed -i "s/__APIINTKEY__/$INTKEY/g" src/gateway/consent-app/apiproxy/resources/node/config.json
 
 #consent-app-transfers
-cp ../consent-app-transfers/apiproxy/resources/node/config.orig ../consent-app-transfers/apiproxy/resources/node/config.json
-sed -i "" "s/__HOST__/$host/g" ../consent-app-transfers/apiproxy/resources/node/config.json
-sed -i "" "s/__APIINTKEY__/$INTKEY/g" ../consent-app-transfers/apiproxy/resources/node/config.json
+cp src/gateway/consent-app-transfers/apiproxy/resources/node/config.orig src/gateway/consent-app-transfers/apiproxy/resources/node/config.json
+sed -i "s/__HOST__/$host/g" src/gateway/consent-app-transfers/apiproxy/resources/node/config.json
+sed -i "s/__APIINTKEY__/$INTKEY/g" src/gateway/consent-app-transfers/apiproxy/resources/node/config.json
 
 # deploy the APIs
-cd ../parent-pom-apps/
+cd src/gateway/parent-pom-apps/
 mvn clean install -Dusername=${ADMIN_EMAIL} -Dpassword=${APW} -Dorg=${ORG} -P${ENV}
-cd ../setup
+cd ../../..
