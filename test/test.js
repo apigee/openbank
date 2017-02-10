af7c3358144a386014068485ec92a29e779d9a2c
Prashanth Subrahmanyam <ksprashanth@google.com>
2017-02-10 17:32:50 +0800

    test framewokr

--- test/test.js
+++ test/test.js
@@ -1,24 +1,26 @@
-var assert = chai.assert;
+describe('DOM Tests', function() {
+  var el = document.createElement('div');
+  el.id = 'myDiv';
+  el.innerHTML = 'Hi there!';
+  el.style.background = '#ccc';
+  document.body.appendChild(el);
 
-var pathname = window.location.pathname
-var splits = pathname.split('/')
-//expected /v1/o/:orgname/e/:env/samples/:sample/test.html
-var org = splits[3]
-var env = splits[5]
-var sample = splits[7]
-var client_id;
-var secret;
+  var myEl = document.getElementById('myDiv');
+  it('is in the DOM', function() {
+    expect(myEl).to.not.equal(null);
 
-describe('write your test casr', function(){
-	before(function(done){
-		done()
-	})
-	it('do something', function(done){
-		var url = 'https://' + org + '-' + env + '.apigee.net'
-			$.ajax({
-				url:url,
-				complete:function(xhr,statusText){ done()},
-				error: function(xhr,err){done(err)}
-			})				
-	})
-})	
+  });
+
+  it('is a child of the body', function() {
+    expect(myEl.parentElement).to.equal(document.body);
+  });
+
+  it('has the right text', function() {
+    expect(myEl.innerHTML).to.equal('Hi there!');
+  });
+
+  it('has the right background', function() {
+    expect(myEl.style.background).to.equal('rgb(204, 204, 204)');
+  });
+
+});
