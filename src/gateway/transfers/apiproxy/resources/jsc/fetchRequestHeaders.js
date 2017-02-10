83fb5e825a78d42eac92e682f7dd12e8917737dd
Prashanth Subrahmanyam <ksprashanth@google.com>
2017-02-10 23:23:10 +0800

    added client creds handling

---
+++ src/gateway/transfers/apiproxy/resources/jsc/fetchRequestHeaders.js
@@ -0,0 +1,15 @@
+var requestHeadersAccept = context.getVariable("request.header.Accept");
+var request_account_number = context.getVariable("request.queryparam.account_number");
+var request_grant_type = context.getVariable("request.queryparam.grant_type");
+context.setVariable("requestHeaderAccept",requestHeadersAccept);
+context.setVariable("requestGrantType",request_grant_type);
+
+if(request_account_number==null || request_account_number=="")
+{
+    context.setVariable("requestAccNum","");
+}
+else
+{
+context.setVariable("requestAccNum",request_account_number);
+}
+
---
+++ src/gateway/transfers/apiproxy/resources/jsc/fetchTransferResponse.js
@@ -0,0 +1,35 @@
+var transferResponse=context.getVariable('CompleteTransferResponse.content');
+transferResponse = JSON.parse(transferResponse);
+var redirectUri=transferResponse.application_tx_response;
+//context.setVariable('jsondata',transferResponse);
+//context.setVariable('jsonuri',redirectUri);
+ var requestHeaderAccept = context.getVariable('requestHeaderAccept');
+if(redirectUri!="" && redirectUri !=null)
+{
+    context.setVariable('status_code',"200");
+   if(requestHeaderAccept.toLowerCase()=="application/javascript")
+    {var res="function transfer(){ return { application_tx_response:"+redirectUri+"} }";
+    context.setVariable('resData',res);
+    }
+    else
+    {
+        
+        var res = "{ application_tx_response:"+redirectUri+"}";
+            context.setVariable('resData',res);
+
+    }
+}
+else
+{
+    context.setVariable('status_code',"400");
+    if(requestHeaderAccept.toLowerCase()=="application/javascript")
+    {var res="function transfer(){ return { error:"+transferResponse.error+", error_description:"+transferResponse.error_description+"} }";
+    context.setVariable('resData',res);
+    }
+    else
+    {
+        var res = "{error:"+transferResponse.error+", error_description:"+transferResponse.error_description+"}";
+         context.setVariable('resData',res);
+    }
+    
+}
