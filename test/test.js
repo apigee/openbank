6c44587b8bd386342f846afd83e465f1fd2bcda5
Prashanth Subrahmanyam <ksprashanth@google.com>
2017-02-13 19:27:21 +0800

    running only payment test

--- test/test.js
+++ test/test.js
@@ -35,13 +35,11 @@ describe('Bank API Tests', function() {
   it('should get account information', function(done) {
     var options = {
       uri: config.hostUrl + '/apis/v1/accounts/' + config.accountId + '/info',
-      auth: {
-        bearer: access_token
-      },
+      auth: {bearer: access_token},
       json: true
     };
 
-    request(options, function (err, res, body) {
+    request(options, function(err, res, body) {
       if (err) console.log(err);
       if (res) console.log(res.statusCode);
       if (body) console.log(body);
@@ -58,14 +56,13 @@ describe('Bank API Tests', function() {
 
   it('should get account balance', function(done) {
     var options = {
-      uri: config.hostUrl + '/apis/v1/accounts/' + config.accountId + '/balance',
-      auth: {
-        bearer: access_token
-      },
+      uri:
+          config.hostUrl + '/apis/v1/accounts/' + config.accountId + '/balance',
+      auth: {bearer: access_token},
       json: true
     };
 
-    request(options, function (err, res, body) {
+    request(options, function(err, res, body) {
       if (err) console.log(err);
       if (res) console.log(res.statusCode);
       if (body) console.log(body);
@@ -80,67 +77,62 @@ describe('Bank API Tests', function() {
     });
   });
 
-  it('should make a payment', function(done) {
+  it.only('should make a payment', function(done) {
     this.timeout(10000);
 
     const jwt = require('jsonwebtoken');
     var payload = {
-      "iss": "https://openbank.apigee.com",
-      "aud": "https://apis-bank-dev.apigee.net",
-      "response_type": "token",
-      "client_id": config.clientId,
-      "redirect_uri": "http://localhost/",
-      "scope": "openid accounts payment",
-      "state": "af0ifjsldkj",
-      "acr_values": "2",
-      "claims": {
-        "paymentinfo": {
-          "type": "sepa_credit_transfer",
-          "to": {
-            "account_number": "62136000",
-            "remote_bic": "RBOSGB2109H",
-            "remote_IBAN": "GB32ESSE40486562136016",
-            "remote_name": "BigZ online store"
+      'iss': 'https://openbank.apigee.com',
+      'aud': 'https://apis-bank-dev.apigee.net',
+      'response_type': 'token',
+      'client_id': config.clientId,
+      'redirect_uri': 'http://localhost/',
+      'scope': 'openid accounts payment',
+      'state': 'af0ifjsldkj',
+      'acr_values': '2',
+      'claims': {
+        'paymentinfo': {
+          'type': 'sepa_credit_transfer',
+          'to': {
+            'account_number': '62136000',
+            'remote_bic': 'RBOSGB2109H',
+            'remote_IBAN': 'GB32ESSE40486562136016',
+            'remote_name': 'BigZ online store'
           },
-          "value": {
-            "currency": "GBP",
-            "amount": "200"
+          'value': {'currency': 'GBP', 'amount': '200'},
+          'additional': {
+            'subject': 'Online Purchase',
+            'booking_code': '2SFBJ28553',
+            'booking_date': '1462517645809',
+            'value_date': '1462517645809'
           },
-          "additional": {
-            "subject": "Online Purchase",
-            "booking_code": "2SFBJ28553",
-            "booking_date": "1462517645809",
-            "value_date": "1462517645809"
-          },
-          "challenge_type": "SANDBOX_TAN"
+          'challenge_type': 'SANDBOX_TAN'
         }
       },
-      "iat": new Date().getTime()
+      'iat': new Date().getTime()
     };
 
     var jwt_token = jwt.sign(payload, config.clientSecret);
-    
+
     var options = {
       uri: config.hostUrl + '/apis/v1/transfers/initiate',
       method: 'POST',
-      auth: {
-        bearer: access_token
-      },
+      auth: {bearer: access_token},
       form: {
-        client_id : config.clientId,
+        client_id: config.clientId,
         redirect_uri: 'http://localhost/',
         scope: 'openid accounts payment',
         'ui-locales': 'en',
         state: 'af0ifjsldkj',
         acr_values: 2,
-        request : jwt_token,
-        grant_type : 'client_credentials',
-        account_from : config.accountId
+        request: jwt_token,
+        grant_type: 'client_credentials',
+        account_from: config.accountId
       },
       json: true
     };
 
-    request(options, function (err, res, body) {
+    request(options, function(err, res, body) {
       if (err) console.log(err);
       if (res) console.log(res.statusCode);
       if (res) console.log(res.headers.location);
