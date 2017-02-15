0b92c07b51af24294db916beb12efd5da7b9b2f8
Prashanth K S <ksprashanth82@gmail.com>
2017-02-15 14:29:58 +0530

    url substitution for transfers

--- test/config.js
+++ test/config.js
@@ -1,7 +1,7 @@
 exports = module.exports = {
-  hostUrl: 'https://apis-bank-dev.apigee.net',
-  clientId: 'grOmvYAei8aBARzEVIYhGtpvztofAAjz',
-  clientSecret: 'SKgHyBAPq9wXjarR',
+  hostUrl: 'https://apisdeploytest-test.apigee.net',
+  clientId: '6jnwNmFvGOI2c0i5QFJEa9pU36Szm3O3',
+  clientSecret: 'lP0yTXyFIabfvQ7p',
   accountId: '2139531801',
   customerId: '123456789'
 };
