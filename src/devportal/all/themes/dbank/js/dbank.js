a3060e3d870b2a6345b2f626b4da326ec679d3f4
Rupam Rai <rrai@apigee.com>
2017-03-28 11:33:58 +0530

    navbar width 100% , GET POST on portal successfull

--- src/devportal/all/themes/dbank/js/dbank.js
+++ src/devportal/all/themes/dbank/js/dbank.js
@@ -16,6 +16,8 @@
 		$("div.title-description > a.active").addClass("activeTitle");
 		$("div.title-description > a.activeTitle").parentsUntil("h3.ui-accordion-header").css("display","block");
 		$("section.block.block-block.clearfix").addClass("home-dbank-block");
+		$("#navbar > div.container").css("width","100%");
+		$("div.verb-auth > p").attr("data-role","verb-role");
 	});
 })(jQuery);
  
