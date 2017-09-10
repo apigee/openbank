47acbfd7371754f4b0dfca95c63151a95e6e9d39
Rohan <rmahalank@google.com>
2017-09-11 03:56:13 +0530

    Added payments proxy

---
+++ src/gateway/payments/apiproxy/resources/jsc/CreateJWS.js
@@ -0,0 +1,22 @@
+/*  Create the jws detached signature   
+*   and pass it in the response header
+*/
+var responsePayload = JSON.parse(context.getVariable("response.content"));
+responsePayload = JSON.stringify(responsePayload);
+var privateKey = context.getVariable("private.privateKey");
+var joseHeader =
+    {
+        "alg": "RS256",
+        "kid": "90210ABAD",
+        "b64": false,
+        "http://openbanking.org.uk/iat": "2017-06-12T20:05:50+00:00",
+        "http://openbanking.org.uk/iss": "C=UK, ST=England, L=London, O=Acme Ltd.",
+        "crit": ["b64", "http://openbanking.org.uk/iat", "http://openbanking.org.uk/iss"]
+
+    };
+
+var jwt = KJUR.jws.JWS.sign(joseHeader.alg, joseHeader, responsePayload, privateKey);
+detachedJWT = jwt.split(".");
+var detachedJws = detachedJWT[0] + ".." + detachedJWT[2];
+print(detachedJWT);
+context.setVariable("detachedJws", detachedJws);
