7370663ea10f60c34595ce73cc6b87f012e82858
Rupam Rai <rairupam@google.com>
2018-03-16 14:03:45 +0530

    datastore-connector explained as Google cloud datastore CRUD interface

--- src/gateway/datastore-connector/apiproxy/resources/jsc/createGetEntityRequest.js
+++ src/gateway/datastore-connector/apiproxy/resources/jsc/createGetEntityRequest.js
@@ -1,3 +1,15 @@
+/*
+ Copyright 2017 Google Inc.
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ https://www.apache.org/licenses/LICENSE-2.0
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
 // GET entity by Id, unique name
 var pathSuffix = ":runQuery";
 var kind = context.getVariable("kind");
@@ -38,8 +50,8 @@ else
     var limit = context.getVariable("request.queryparam.limit");
     var cursor = context.getVariable("request.queryparam.cursor");
     var queryString = "SELECT * FROM "+ kind;
-    var dsReqQuery = unescape(context.getVariable("request.queryparam.ql"));
-    if(dsReqQuery && dsReqQuery!= "" )
+    var dsReqQuery = context.getVariable("request.queryparam.ql");
+    if(dsReqQuery && dsReqQuery!=null && dsReqQuery!= "" )
     {
       queryString += " " + dsReqQuery;
     }
